ed9f17eef8699b97a203cbd7f9b5ad1c4f2c2401
Yassine <66925471+ymefath@users.noreply.github.com>
2024-10-31 00:40:01 +0100

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000;
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
-app.get('', async (req, res) => {
+app.get('/api/products', async (req, res) => {
     try {
         await client.connect();
         const database = client.db("prodata"); // Replace "prodata" with your actual database name if different
