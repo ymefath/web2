dfe82725e26945a3a1a70e2c91a352a8b91f47cd
Yassine <66925471+ymefath@users.noreply.github.com>
2024-10-30 23:15:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const app = express();
-const port = process.env.PORT || 3000; // Use environment variable for PORT or default to 3000
+const port = 3000;
 
-// Use environment variable for MongoDB connection string
-const uri = process.env.MONGODB_URI;
+// MongoDB connection string
+const uri = "mongodb+srv://yassine:12345yassine@cluster0.rhfeo.mongodb.net/prodata";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+// Define an API endpoint to retrieve product data
 app.get('/api/products', async (req, res) => {
     try {
         await client.connect();
-        const database = client.db("prodata"); // Replace with your database name if necessary
+        const database = client.db("prodata");
         const collection = database.collection("serb");
 
         // Fetch all products from the 'serb' collection
         const products = await collection.find({}).toArray();
-        res.json(products);
+        res.json(products);  // Send the data as JSON
     } catch (error) {
         console.error('Error fetching data:', error);
         res.status(500).json({ error: 'Failed to fetch products' });
